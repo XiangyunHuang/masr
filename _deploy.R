3185e0777968a9841f48402f3aef69018cc2665f
xiangyun <xiangyunfaith@outlook.com>
2022-06-10 11:41:12 +0800

    Quarto Book 需要 quarto 包部署

--- _deploy.R
+++ _deploy.R
@@ -4,9 +4,9 @@ rsconnect::connectApiUser(
   account = "xiangyun", server = "bookdown.org",
   apiKey = Sys.getenv("CONNECT_API_KEY")
 )
-rsconnect::deploySite(
-  appId = Sys.getenv("CONTENT_ID"),
+quarto::quarto_publish_site(
+  name = "masr", render = "none",
   server = "bookdown.org",
-  render = "none", logLevel = "verbose",
-  forceUpdate = TRUE
+  account = "xiangyun",
+  title = "现代应用统计"
 )
